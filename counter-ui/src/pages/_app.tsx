5e829a5d1fc92f6ed444c4c36af829a8d64f465d
erick <erickrs2esuarez@gmail.com>
2025-06-09 13:55:35 -0300

    wallet integration

--- counter-ui/src/pages/_app.tsx
+++ counter-ui/src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { MidnightMeshProvider } from "@/components/midnight/wallet-widget";
+import * as pino from "pino";
+
+export const logger = pino.pino({
+  level: "trace",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MidnightMeshProvider logger={logger}><Component {...pageProps} /></MidnightMeshProvider>
+  );
 }
